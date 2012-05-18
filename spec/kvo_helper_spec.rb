cf5be30ee7dd49195381eb4fa6b7b8ed9126e525
Jan Weinkauff <jan.weinkauff@me.com>
2012-05-18 14:59:37 +0200

    added spec example to proof that it is possible to use more than one block on the same target and key path

--- spec/kvo_helper_spec.rb
+++ spec/kvo_helper_spec.rb
@@ -42,4 +42,26 @@ describe "KVOHelper" do
     observed.should == true
   end
   
+  it "should be able to observe a key path with more than one block" do
+    observed_one = false
+    observed_two = false
+    observed_three = false
+    
+    @example.observe_label do |label, old_value, new_value|
+      observed_one = true
+    end
+    @example.observe_label do |label, old_value, new_value|
+      observed_two = true
+    end
+    @example.observe_label do |label, old_value, new_value|
+      observed_three = true
+    end
+    
+    @example.label.text = "Bar"
+    
+    observed_one.should == true
+    observed_two.should == true
+    observed_three.should == true
+  end
+  
 end
